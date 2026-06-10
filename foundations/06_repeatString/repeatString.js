537c3dafbc3a39989bc8e389b6889ab08b4509ec
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-10 17:43:11 -0400

    Completed exercise 7 of js-foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    let repeatedString = "";
+    
+    if (numTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < numTimes; i++) {
+            repeatedString += string;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
