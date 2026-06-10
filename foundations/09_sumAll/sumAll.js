79b1f276acd472938bfd2ebe2e5618e03d51ae95
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-10 19:23:46 -0400

    Completed exercise 9 of js-foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if (!Number.isInteger(num1) || 
+        !Number.isInteger(num2) ||
+        num1 < 0 ||
+        num2 < 0) {
+        return "ERROR";
+    } else {
+        if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                total += i;
+            }
+        } else {
+            for (let i = num1; i <= num2; i++) {
+                total += i;
+            }
+        }
+    }
 
+    return total;
 };
 
 // Do not edit below this line
