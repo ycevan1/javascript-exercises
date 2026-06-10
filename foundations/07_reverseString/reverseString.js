dbf7061a3f13e0ea3524efc75bb36d9a43b634ab
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-10 17:53:30 -0400

    Completed exercise 7 of js-foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    
+    for (let i = 0; i < string.length; i++) {
+        reversedString = string[i] + reversedString
+    }
 
+    return reversedString
 };
 
 // Do not edit below this line
