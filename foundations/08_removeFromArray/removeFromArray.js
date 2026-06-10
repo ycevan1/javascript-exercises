4a69a42c0bdba5f8c48054827d76e5b6cd9b4b60
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-10 18:48:47 -0400

    Completed exercise 8 of js-foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+    
+    for (let value of removedValues) {
+        array = array.filter((arrayValue) => arrayValue !== value);
+    }
+    return array;
 };
 
 // Do not edit below this line
