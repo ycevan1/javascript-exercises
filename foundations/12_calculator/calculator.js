db7cf4139f4cd9afd76b7913fdaf92f13812e794
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-21 04:17:57 -0400

    Completed exercise 12 of js-foundations

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0;
+  }
+  
+	return array.reduce((total, num) => {
+    return total + num;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => {
+    return total * num;
+  })
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
