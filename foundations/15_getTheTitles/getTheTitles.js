ca9fbc609eec3775e42b6aa05c0074f535ead08b
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-22 02:26:03 -0400

    Completed exercise 15 of js-foundations

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map((book) => {
+        return book.title;
+    });
 };
 
 // Do not edit below this line
