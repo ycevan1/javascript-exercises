9d3f41c79a828eda87ed45a43978243ad85d6e09
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-22 03:14:32 -0400

    Completed exercise 16 of js-foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson;
+    people.forEach((person) => {
+        let deathYear;
+        if (person.yearOfDeath == undefined) {
+            deathYear = new Date().getFullYear();
+        } else {
+            deathYear = person.yearOfDeath;
+        }
 
+        if (deathYear - person.yearOfBirth > oldestAge) {
+            oldestPerson = person;
+            oldestAge = deathYear - person.yearOfBirth;
+        }
+    })
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
