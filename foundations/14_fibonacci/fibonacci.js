ea0a551dd3d99e02b62f8b3f3c569df5128a6a38
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-22 01:01:36 -0400

    Completed exercise 14 of js-foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibNum1 = 1;
+    let fibNum2 = 1;
 
+    if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 2; i < num; i++) {
+        temp = fibNum2;
+        fibNum2 = fibNum1 + fibNum2;
+        fibNum1 = temp;
+    }
+
+    return fibNum2;
 };
 
 // Do not edit below this line
