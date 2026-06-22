70f79934616fd968e72c0cf3b66e36f773cfd150
ycevan1 <157036852+ycevan1@users.noreply.github.com>
2026-06-21 23:44:38 -0400

    Completed exercise 13 of js-foundations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedString = string.replaceAll(" ", "").replaceAll(/[.,!]/g, "").toLowerCase();
+    console.log(cleanedString);
+    for (let i = 0; i < Math.floor((cleanedString.length / 2)); i++) {
+        if (cleanedString[i] != cleanedString[cleanedString.length - 1 - i]) {
+            console.log(i);
+            console.log(cleanedString[i]);
+            console.log(cleanedString[cleanedString.length - 1 -i]);
+            return false;
+        } 
+    }
+    return true;
 };
 
 // Do not edit below this line
